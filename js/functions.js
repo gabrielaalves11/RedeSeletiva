d82f8d8274ab96aa162398f1d4ea55d25bd63045
lucas <lucaslmota0431@gmail.com>
2020-09-09 18:11:03 -0300

    Alteração final Gabi e lucas

--- js/functions.js
+++ js/functions.js
@@ -1,10 +1,7 @@
-// Exemplo de JavaScript inicial para desativar envios de formulário, se houver campos inválidos.
 (function () {
     'use strict';
     window.addEventListener('load', function () {
-        // Pega todos os formulários que nós queremos aplicar estilos de validação Bootstrap personalizados.
         var forms = document.getElementsByClassName('needs-validation');
-        // Faz um loop neles e evita o envio
         var validation = Array.prototype.filter.call(forms, function (form) {
             form.addEventListener('submit', function (event) {
                 if (form.checkValidity() === false) {
@@ -56,41 +53,6 @@ function validar(){
     let link = document.getElementById('linkDesabilitado')
 
     if(email.value.length == 0 && senha.value.length == 0){
-        window.alert('[ERRO] Os campos tem que estar preenchidos.')
-        link.href = ''
-    }else{
-        link.href = "pag_inicial.html"
-    }
-
-}
-
-/*!function (t) {
-    "use strict";
-    t("#sidebarToggle, #sidebarToggleTop").on("click",
-
-        function (o) {
-            t("body").toggleClass("sidebar-toggled"),
-                t(".sidebar").toggleClass("toggled"),
-                t(".sidebar").hasClass("toggled") && t(".sidebar .collapse").collapse("hide")
-        }),
-        t(window).resize(function () {
-            t(window).width() < 768 && t(".sidebar .collapse").collapse("hide")
-        }),
-        t("body.fixed-nav .sidebar").on("mousewheel DOMMouseScroll wheel",
-            function (o) {
-                if (768 < t(window).width()) {
-                    var e = o.originalEvent, l = e.wheelDelta || -e.detail;
-                    this.scrollTop += 30 * (l < 0 ? 1 : -1), o.preventDefault()
-                }
-            }), t(document).on("scroll", function () { 100 < t(this).scrollTop() ? t(".scroll-to-top").fadeIn() : t(".scroll-to-top").fadeOut() }), t(document).on("click", "a.scroll-to-top", function (o) { var e = t(this); t("html, body").stop().animate({ scrollTop: t(e.attr("href")).offset().top }, 1e3, "easeInOutExpo"), o.preventDefault() })
-}(jQuery);
-
-function validar(){
-    let email = document.getElementById('email-entrada')
-    let senha = document.getElementById('senha-de-entrada')
-    let link = document.getElementById('linkDesabilitado')
-
-    if(email.value.length == 0 || senha.value.length == 0){
         window.alert('[ERRO] Os campos tem que estar preenchidos.')
         link.href = ''
     }else{
